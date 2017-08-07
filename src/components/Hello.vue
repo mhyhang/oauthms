f5a6fe01b6c829404f6c133b2b24f3ee94d10636
lossa <luomusha@gmail.com>
2017-08-07 14:08:56 +0800

    init

---
+++ src/components/Hello.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <div>
+      <label>账号:<input type="text"></label>
+    </div>
+    <div>
+      <label>密码:<input type="password"></label>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'hello',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
